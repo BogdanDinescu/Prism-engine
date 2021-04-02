e2154e466e5a83a0b3045a388b6c0b5330ba0be3
Bogdan <bogdandinescu@tutanota.com>
2021-04-02 22:44:01 +0300

    Get news updated

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -44,7 +44,7 @@ namespace Prism.Controllers
             return Ok(new
             {
                 news = database.NewsArticles.Where(x => sources.Contains(x.NewsSource))
-                    .OrderBy(x => x.SimHash).Select(x => new { x.Id, x.Title, x.Source, x.Content, x.ImageUrl, x.Link, x.SimHash})
+                    .OrderByDescending(x => x.Group).Select(x => new { x.Id, x.Title, x.Source, x.Content, x.ImageUrl, x.Link, x.CreateDate, x.Group})
                     .Skip(page*20).Take(20).ToList()
             });
         }
